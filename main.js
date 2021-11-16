481915740fa6cec78dd9cee7c3710a663fee7463
Gaurav <gauravboob5@gmail.com>
2021-11-16 09:47:09 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert(inputValue);
+});
+
